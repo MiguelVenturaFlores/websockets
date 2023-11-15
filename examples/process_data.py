822b27725c27f2827d27ca1295ad3cf345f36c31
MiguelVenturaFlores <miguel.angel.venturaf@gmail.com>
2023-11-15 19:19:15 +0100

    add examples

--- examples/process_data.py
+++ examples/process_data.py
@@ -1,11 +1,33 @@
 from websockets.data_processing import *
 
+PAIRS = [
+    "adausdt",
+    "bnbusdt",
+    "btcusdt",
+    "dogeusdt",
+    "ethusdt",
+    "solusdt",
+    "trxusdt",
+    "xrpusdt"
+]
+
 if __name__ == "__main__":
 
     # input
-    depth = 2
+    depth = 5
     base_path = "."
-    pair = "btcusdt"
-    date = "20231113"
+    date = "20231115"
 
-    process_data(depth, pair, date, base_path)
+    for pair in PAIRS:
+
+        print(f"\n\nProcessing pair {pair}...")
+
+        # process data
+        process_data(depth, pair, date, base_path)
+
+        # validate data
+        snapshots_path = f"raw_data/{pair}/{date}/take_1/orderbook/"
+        true_ob = get_snapshots(snapshots_path, depth)
+        ob_path = f"preprocessed_data/{pair}/{date}/orderbook/take_1.csv"
+        computed_ob = pd.read_csv(ob_path).drop(columns=["Unnamed: 0"])
+        compare_orderbook(true_ob, computed_ob)
